a478596c2c20504c450a4ce56ebfda275553611d
Mikhail <mikhailkhudoshin@gmail.com>
2022-04-02 06:52:25 +0400

    feat: add productItem + Cart + redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import 'font-awesome/css/font-awesome.min.css';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
@@ -11,7 +13,9 @@ import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+  </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
